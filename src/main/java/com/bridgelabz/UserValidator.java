cf774c3892a0bdda1e3b78fefcf1cf31807508cb
Megha <meghajagadale99@gmail.com>
2021-08-26 10:15:28 +0530

    Added: valid first name

--- src/main/java/com/bridgelabz/UserValidator.java
+++ src/main/java/com/bridgelabz/UserValidator.java
@@ -6,7 +6,7 @@ public class UserValidator {
     public static void main(String[] args) {
         UserRegistrationTest nameObj = new UserRegistrationTest();
 
-        System.out.println("Enter First Name:");
+        System.out.println("Enter a First Name:");
         Scanner sc = new Scanner(System.in);
         String firstName = sc.next();
         nameObj.firstnameVerification(firstName);
