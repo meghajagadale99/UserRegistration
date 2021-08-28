b3e2599ea978af1988e0edad57fd9034d947b133
Megha <meghajagadale99@gmail.com>
2021-08-28 15:06:58 +0530

    Added: pattern valid for firstname

--- src/main/java/com/bridgelabz/UserRegistrationTest.java
+++ src/main/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,73 +1,19 @@
 package com.bridgelabz;
 
-import java.util.regex.Matcher;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
+
 public class UserRegistrationTest {
-    String first_name;
-    String last_name;
-    String email;
-    String mo_number;
-    String password;
+    public void checkPatternFirstName() {
+        String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("enter the First Name: ");
+        String checkPattern = userInput.next();
 
-    //Verify firstname
-    public void firstnameVerification(String first_name) {
-        Pattern pattern_first_name = Pattern.compile("[A-Z][a-z]{2,}");
-        this.first_name = first_name;
-        Matcher firstName_matcher = pattern_first_name.matcher(first_name);
-
-        if (firstName_matcher.find() && firstName_matcher.group().equals(first_name))
-            System.out.println("valid name");
+        if (Pattern.matches(firstNamePattern, checkPattern))
+            System.out.println("Pattern match. Correct Input.");
         else
-            System.out.println("Invalid name");
+            System.out.println("pattern match fail!\nFirst name starts with Cap and has minimum 3 characters");
     }
-
-    //verify lastName
-    public void lastnameVerification(String last_name) {
-        Pattern pattern_last_name = Pattern.compile("[A-Z][a-z]{2,}");
-        this.last_name = last_name;
-        Matcher lastName_matcher = pattern_last_name.matcher(last_name);
-
-        if (lastName_matcher.find() && lastName_matcher.group().equals(last_name))
-            System.out.println("valid name");
-        else
-            System.out.println("Invalid name");
-    }
-
-    //verify email
-    public void emailVerify(String email) {
-        Pattern pattern_email = Pattern.compile("[A-Za-z0-9]{1,}[.][A-Za-z0-9]{1,}@[A-Z,a-z]{1,}[.]com[.]in");
-        this.email = email;
-        Matcher email_matcher = pattern_email.matcher(email);
-
-        if (email_matcher.find() && email_matcher.group().equals(email))
-            System.out.println("valid Email");
-        else
-            System.out.println("Invalid Email");
-    }
-
-    //mobile number
-    public void mobileFormat(String mo_number) {
-        Pattern pattern_mo_number = Pattern.compile("(0|91)?[7-9][0-9]{9}");
-        this.mo_number = mo_number;
-        Matcher mo_number_matcher = pattern_mo_number.matcher(mo_number);
-
-        if (mo_number_matcher.find() && mo_number_matcher.group().equals(mo_number))
-            System.out.println("valid Mobile Number");
-        else
-            System.out.println("Invalid Mobile Number");
-    }
-
-    //check password has at least one upperCase, one numeric value and exactly one special character
-    public void passwordVerify(String password) {
-        Pattern pattern_password = Pattern.compile("^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*$");
-        this.password = password;
-        Matcher password_matcher = pattern_password.matcher(password);
-
-        if(password_matcher.find() &&  password_matcher.group().equals(password))
-            System.out.println("valid password");
-        else
-            System.out.println("Invalid password");
-    }
-
 }
--- src/main/java/com/bridgelabz/UserValidator.java
+++ src/main/java/com/bridgelabz/UserValidator.java
@@ -1,34 +1,13 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
-
-public class UserValidator {
+public class  UserValidator {
     public static void main(String[] args) {
-        UserRegistrationTest nameObj = new UserRegistrationTest();
+        System.out.println("Welcome to user registration file.");
 
-        System.out.println("Enter a First Name:");
-        Scanner sc = new Scanner(System.in);
-        String firstName = sc.next();
-        nameObj.firstnameVerification(firstName);
+        UserRegistrationTest firstName = new UserRegistrationTest();
 
-        System.out.println("Enter a Last Name:");
-        Scanner sc1 = new Scanner(System.in);
-        String lastName = sc1.next();
-        nameObj.lastnameVerification(lastName);
+        firstName.checkPatternFirstName();
 
-        System.out.println("Enter a Email:");
-        Scanner sc3 = new Scanner(System.in);
-        String email = sc3.next();
-        nameObj.emailVerify(email);
-
-        System.out.println("Enter Mobile Number:");
-        Scanner sc4 = new Scanner(System.in);
-        String mo_number = sc4.next();
-        nameObj.mobileFormat(mo_number);
-
-        System.out.println("Enter password:");
-        Scanner sc5 = new Scanner(System.in);
-        String password = sc5.next();
-        nameObj.passwordVerify(password);
     }
+
 }
