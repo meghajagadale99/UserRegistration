2c10302cd5b754e71bedd5014ed8a37333e54b64
Megha <meghajagadale99@gmail.com>
2021-08-28 20:00:44 +0530

    Refactor: throw custom exception

--- src/main/java/com/bridgelabz/UserRegistrationTest.java
+++ src/main/java/com/bridgelabz/UserRegistrationTest.java
@@ -6,27 +6,48 @@ import java.util.ArrayList;
 
 public class UserRegistrationTest {
 
-    public void checkPatternFirstName() {
-        String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("enter the First Name: ");
-        String checkPattern = userInput.next();
+    final String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+    final String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+    final String emailIdPattern = "^[a-zA-Z0-9]{1,}[.a-zA-Z0-9]*@[a-zA-Z0-9]{1,}((.){1}+)([a-z]{1,3}+)(.[a-z]{2})*$";
+    final String phoneNumberPattern = "[0-9]{2}[\\s][0-9]{10}$";
+    final String passwordPattern = "^(?=.*\\d)([a-z])*(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$";
 
-        if (Pattern.matches(firstNamePattern, checkPattern))
-            System.out.println("Pattern match. Correct Input.");
-        else
-            System.out.println("pattern match fail!\nFirst name starts with Cap and has minimum 3 characters");
+    public void checkPatternFirstName() {
+
+        try {
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("enter the First Name: ");
+            String checkPattern = userInput.nextLine();
+
+            if (Pattern.matches(firstNamePattern, checkPattern))
+                System.out.println("Pattern match. Correct Input.");
+            else
+                throw new UserDefineException("Please Follow the pattern And Enter a valid Frist name");
+            System.out.println("First name:- " + checkPattern);
+        }
+
+        catch (UserDefineException e) {
+            System.out.println(e.errorMessage);
+            checkPatternFirstName();
+        }
     }
     public void checkPatternLastName() {
-        String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("enter the Last Name: ");
-        String checkPattern = userInput.next();
 
-        if (Pattern.matches(lastNamePattern, checkPattern))
-            System.out.println("Pattern match.");
-        else
-            System.out.println("pattern match fail! \nLast name starts with Cap and has minimum 3 characters");
+        try {
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("enter the Last Name: ");
+            String checkPattern = userInput.next();
+
+            if (Pattern.matches(lastNamePattern, checkPattern))
+                System.out.println("Pattern match.");
+            else
+                throw new UserDefineException("Please follow the pattern and enter a valid LastName");
+            System.out.println("Last name:- " + checkPattern);
+        }
+        catch (UserDefineException e) {
+            System.out.println(e.errorMessage);
+            checkPatternLastName();
+        }
     }
     public void checkPatternEmailId() {
 
@@ -56,36 +77,56 @@ public class UserRegistrationTest {
         emails.add("abc@gmail.com.1a");
         emails.add("abc@gmail.com.aa.au");
 
-        String emailIdPattern = "^[a-zA-Z0-9]{1,}[.a-zA-Z0-9]*@[a-zA-Z0-9]{1,}((.){1}+)([a-z]{1,3}+)(.[a-z]{2})*$";
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("enter the Email address: ");
-        String checkPattern = userInput.next();
+        try {
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("enter the Email address: ");
+            String checkPattern = userInput.next();
 
-        if (Pattern.matches(emailIdPattern, checkPattern))
-            System.out.println("Pattern match.");
-        else
-            System.out.println("pattern match fail!Email has 3 mandatory parts abc, bl, & co and 2 optional (xyz & in) with	precise @ and . positions");
+            if (Pattern.matches(emailIdPattern, checkPattern))
+                System.out.println("Pattern match.");
+            else
+                throw new UserDefineException("Please follow the pattern and enter a valid Email");
+            System.out.println("Email:- " + checkPattern);
+        }
+        catch (UserDefineException e) {
+            System.out.println(e.errorMessage);
+            checkPatternEmailId();
+        }
     }
     public void checkPatternPhoneNumber() {
-        String phoneNumberPattern = "[0-9]{2}[\\s][0-9]{10}$";
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("enter the PhoneNo: ");
-        String checkPattern = userInput.nextLine();
 
-        if (Pattern.matches(phoneNumberPattern, checkPattern))
-            System.out.println("Pattern match.");
-        else
-            System.out.println("pattern match fail!\nCountry code follow by space and 10 digit number");
+        try {
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("enter the PhoneNo: ");
+            String checkPattern = userInput.nextLine();
+
+            if (Pattern.matches(phoneNumberPattern, checkPattern))
+                System.out.println("Pattern match.");
+            else
+                throw new UserDefineException("Please follow the pattern and enter a valid PhoneNumber");
+            System.out.println("Phone Number:- " + checkPattern);
+        }
+        catch (UserDefineException e) {
+            System.out.println(e.errorMessage);
+            checkPatternPhoneNumber();
+        }
     }
     public void checkPatternPassword() {
-        String passwordPattern = "^(?=.*\\d)([a-z])*(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$";
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("enter the Password: ");
-        String checkPattern = userInput.next();
 
-        if (Pattern.matches(passwordPattern, checkPattern))
-            System.out.println("Pattern match.");
-        else
-            System.out.println("pattern match fail! \nplease follow the pattern for right password");
+        try {
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("enter the Password: ");
+            String checkPattern = userInput.next();
+
+            if (Pattern.matches(passwordPattern, checkPattern))
+                System.out.println("Pattern match.");
+            else
+                throw new UserDefineException("Please follow the pattern and enter a valid Password");
+            System.out.println("Password:- " + checkPattern);
+        }
+        catch (UserDefineException e) {
+            System.out.println(e.errorMessage);
+            checkPatternPassword();
+        }
     }
 }
